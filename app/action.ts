0ca51dc9583c463aee1ce721d521352561fcd352
Romi Julianto <romyjulians@gmail.com>
2023-12-26 15:36:40 +0700

    feat: add server action

---
+++ app/action.ts
@@ -0,0 +1,9 @@
+"use server";
+
+export const fecthAnime = async (page: number) => {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+  );
+  const data = await response.json();
+  return data;
+};
